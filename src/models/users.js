69139ceab569848b4d1cbd45f37d559a7d6724e4
Manatees <manatees@live.cn>
2018-03-29 00:06:46 +0800

    first commit

---
+++ src/models/users.js
@@ -0,0 +1,41 @@
+import * as usersService from '../services/users';
+
+import queryString from 'query-string';
+// const queryString = require('query-string');
+export default {
+  namespace: 'users',
+
+  state: {
+    list: [],
+    total: null,
+    page: null,
+  },
+
+  reducers: {
+    save(state, {payload: {data: list, total, page } }) {
+      return { ...state, list, total, page };
+    },
+  },
+  effects: {
+    *fetch({ payload: { page = 1 } }, { call, put }) {
+      const { data, headers } = yield call(usersService.fetch, {page});
+      yield put({
+        type: 'save',
+        payload: {
+          data,
+          total: parseInt(headers['x-total-count'], 10),
+          page: parseInt(page, 10),
+        },
+      });
+    },
+  },
+  subscriptions: {
+    setup({ dispatch, history }) {
+      return history.listen(({ pathname, query }) => {
+        if (pathname === '/users') {
+          dispatch({ type: 'fetch', payload: queryString.parse(query) });
+        }
+      });
+    },
+  },
+};

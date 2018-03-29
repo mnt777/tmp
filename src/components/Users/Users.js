887a6301e78f0132d88fa552cc5cf94ffd8254b7
manatee <manatees@live.cn>
2018-03-29 16:41:12 +0800

    completed.

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import {connect} from 'dva';
-import {Table, Pagination, Popconfirm } from 'antd';
+import {Table, Pagination, Popconfirm, Button } from 'antd';
 import { routerRedux } from 'dva/router';
 import styles from './Users.css';
 import { PAGE_SIZE } from '../../constants'
 import queryString from 'query-string';
+import UserModal from './UserModal';
 
 function Users({ dispatch, list: dataSource, loading, total, page: current }) {
   function deleteHandler(id) {
-    console.warn(`TODO: ${id}`);
+    // console.warn(`TODO: ${id}`);
+    dispatch({
+      type: 'users/remove',
+      payload: id,
+    });
   }
   
   function pageChangeHandler(page) {
@@ -16,6 +21,20 @@ function Users({ dispatch, list: dataSource, loading, total, page: current }) {
       pathname: '/users',      
       search: queryString.stringify({ page })
     }))
+  }
+
+  function editHandler(id, values) {
+    dispatch({
+      type: 'users/patch',
+      payload: { id, values },
+    });
+  }
+
+  function createHandler(values) {
+    dispatch({
+      type: 'users/create',
+      payload: values
+    });
   }
 
   const columns = [
@@ -38,10 +57,13 @@ function Users({ dispatch, list: dataSource, loading, total, page: current }) {
     {
       title: 'Operation',
       key: 'operation',
-      render: (text, {id}) => (
+      render: (text, record ) => (
         <span className={styles.operation}>
-          <a href="">Edit</a>
-          <Popconfirm title="Confirm to delete?" onConfirm={deleteHandler.bind(null, id)}>
+          <UserModal record={record} onOK={editHandler.bind(null, record.id)}>
+            <a>Edit</a>
+          </UserModal>
+
+          <Popconfirm title="Confirm to delete?" onConfirm={deleteHandler.bind(null, record.id)}>
             <a href="">Delete</a>
           </Popconfirm>
         </span>
@@ -52,6 +74,11 @@ function Users({ dispatch, list: dataSource, loading, total, page: current }) {
   return (
     <div className={styles.normal}>
       <div>
+        <div className={styles.create}>
+          <UserModal record={{}} onOK={createHandler}>
+            <Button type="primary">Create User</Button>
+          </UserModal>
+        </div>
         <Table
           columns={columns}
           dataSource={dataSource}
